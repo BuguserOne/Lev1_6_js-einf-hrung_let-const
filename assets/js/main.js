99f24a5c8138048b0e22b4fadc38e0a7f6a16442
BuguserOne <jinlingone@gmail.com>
2023-01-04 15:28:40 +0100

    firstCommit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+//Deklariere die Variable let x mit dem Wert 34.
+let x = 34;
+//Überschreibe x anschließend mit dem Wert 67.
+x = 67;
+//Gib den Wert x in der Konsole aus.
+console.log(x);
+
+// let kann man ändern (wert)
+
+//Deklariere die Variable const y mit dem Wert 34.
+const y = 34
+//Überschreibe y anschließend mit dem Wert 67.
+y = 67;
+//Gib den Wert y in der Konsole aus.
+console.log(y);
+
+//const kann man nicht ändern -> ERROR
